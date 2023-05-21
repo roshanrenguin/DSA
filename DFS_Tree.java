098c17c4ca4afba432249688d61d92a976d5a33b
roshanrenguin <95216321+roshanrenguin@users.noreply.github.com>
2023-05-21 12:42:53 +0530

    Update DFS_Tree.java

--- DFS_Tree.java
+++ DFS_Tree.java
@@ -1,1 +1,45 @@
 
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+public class DFS_Tree {
+    Node root;
+
+    void depthFirstSearch() {
+        if (root == null) {
+            System.out.println("Tree is empty.");
+            return;
+        }
+
+        dfsUtil(root);
+    }
+
+    void dfsUtil(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.data + " ");
+        dfsUtil(node.left);
+        dfsUtil(node.right);
+    }
+
+    public static void main(String[] args) {
+    	DFS_Tree tree = new DFS_Tree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.print("DFS traversal of the binary tree: ");
+        tree.depthFirstSearch();
+    }
+}
