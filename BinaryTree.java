21930488da9996910ab283d5dbb6ca941a35c671
roshanrenguin <95216321+roshanrenguin@users.noreply.github.com>
2023-05-21 12:41:10 +0530

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,69 @@
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    BinaryTree() {
+        root = null;
+    }
+
+    // Preorder traversal
+    void preorderTraversal(Node node) {
+        if (node == null)
+            return;
+
+        System.out.print(node.data + " ");
+        preorderTraversal(node.left);
+        preorderTraversal(node.right);
+    }
+
+    // Inorder traversal
+    void inorderTraversal(Node node) {
+        if (node == null)
+            return;
+
+        inorderTraversal(node.left);
+        System.out.print(node.data + " ");
+        inorderTraversal(node.right);
+    }
+
+    // Postorder traversal
+    void postorderTraversal(Node node) {
+        if (node == null)
+            return;
+
+        postorderTraversal(node.left);
+        postorderTraversal(node.right);
+        System.out.print(node.data + " ");
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.print("Preorder traversal: ");
+        tree.preorderTraversal(tree.root);
+        System.out.println();
+
+        System.out.print("Inorder traversal: ");
+        tree.inorderTraversal(tree.root);
+        System.out.println();
+
+        System.out.print("Postorder traversal: ");
+        tree.postorderTraversal(tree.root);
+        System.out.println();
+    }
+}
